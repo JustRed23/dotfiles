a9df0eb738d2b4329d366b1eaca9336db699d176
JustRed23 <manuel.bormans@gmail.com>
2025-10-26 00:13:54 +0200

    Fix crackling issues with my USB headset

--- system/base.nix
+++ system/base.nix
@@ -22,6 +22,14 @@
 	    alsa.enable = true;
 	    alsa.support32Bit = true;
 	    pulse.enable = true;
+	    extraConfig.pipewire."92-low-latency" = {
+      		"context.properties" = {
+        	    "default.clock.rate" = 48000;
+        	    "default.clock.quantum" = 512;
+        	    "default.clock.min-quantum" = 512;
+        	    "default.clock.max-quantum" = 2048;
+        	};
+     	    };
 	};
 
 	pulseaudio.enable = false;
